0d0767bf158658150365a329a750c5b9019e8926
Julien Fontanet <julien.fontanet@isonoe.net>
2021-10-28 09:42:48 +0200

    feat: expose synchronize and synchronizeMethod

--- src/index.js
+++ src/index.js
@@ -1,3 +1,83 @@
+const DEFAULT_KEY_FUNCTION = arg => arg;
+
+export function synchronize() {
+  let queue = Promise.resolve();
+  return fn =>
+    function() {
+      const makeCall = () => fn.apply(this, arguments);
+
+      return (queue = queue.then(makeCall, makeCall));
+    };
+}
+synchronize.withKey = function synchronizeWithKey(
+  keyFunction = DEFAULT_KEY_FUNCTION
+) {
+  const queues = new Map();
+  return fn =>
+    function() {
+      const key = keyFunction.apply(this, arguments);
+      const makeCall = () => fn.apply(this, arguments);
+      let queue = queues.get(key);
+      queues.set(
+        key,
+        (queue =
+          queue === undefined ? makeCall() : queue.then(makeCall, makeCall))
+      );
+      const clean = () => {
+        if (queues.get(key) === queue) {
+          queues.delete(key);
+        }
+      };
+      queue.then(clean, clean);
+      return queue;
+    };
+};
+
+export function synchronizeMethod() {
+  const queues = new WeakMap();
+  return method =>
+    function() {
+      const makeCall = () => method.apply(this, arguments);
+
+      let queue = queues.get(this);
+      queues.set(
+        this,
+        (queue =
+          queue === undefined ? makeCall() : queue.then(makeCall, makeCall))
+      );
+      return queue;
+    };
+}
+synchronizeMethod.withKey = function synchronizeMethodWithKey(
+  keyFunction = DEFAULT_KEY_FUNCTION
+) {
+  const instancesQueues = new WeakMap();
+  return method =>
+    function() {
+      const key = keyFunction.apply(this, arguments);
+      let queues = instancesQueues.get(this);
+      if (queues === undefined) {
+        instancesQueues.set(this, (queues = new Map()));
+      }
+      const makeCall = () => method.apply(this, arguments);
+      let queue = queues.get(key);
+      queues.set(
+        key,
+        (queue =
+          queue === undefined ? makeCall() : queue.then(makeCall, makeCall))
+      );
+      const clean = () => {
+        if (queues.get(key) === queue) {
+          queues.delete(key);
+        }
+      };
+      queue.then(clean, clean);
+      return queue;
+    };
+};
+
+// -------------------------------------------------------------------
+
 const toDecorator = (wrapFn, wrapMd = wrapFn) => (...args) => {
   const wrapFn_ = wrapFn(...args);
   const wrapMd_ = wrapMd(...args);
@@ -12,84 +92,8 @@ const toDecorator = (wrapFn, wrapMd = wrapFn) => (...args) => {
         };
 };
 
-// ===================================================================
-
-export const synchronized = toDecorator(
-  () => {
-    let queue = Promise.resolve();
-    return fn =>
-      function() {
-        const makeCall = () => fn.apply(this, arguments);
-
-        return (queue = queue.then(makeCall, makeCall));
-      };
-  },
-  () => {
-    const queues = new WeakMap();
-    return method =>
-      function() {
-        const makeCall = () => method.apply(this, arguments);
-
-        let queue = queues.get(this);
-        queues.set(
-          this,
-          (queue =
-            queue === undefined ? makeCall() : queue.then(makeCall, makeCall))
-        );
-        return queue;
-      };
-  }
-);
-
-// -------------------------------------------------------------------
-
-const DEFAULT_KEY_FUNCTION = arg => arg;
-
+export const synchronized = toDecorator(synchronize, synchronizeMethod);
 synchronized.withKey = toDecorator(
-  (keyFunction = DEFAULT_KEY_FUNCTION) => {
-    const queues = new Map();
-    return fn =>
-      function() {
-        const key = keyFunction.apply(this, arguments);
-        const makeCall = () => fn.apply(this, arguments);
-        let queue = queues.get(key);
-        queues.set(
-          key,
-          (queue =
-            queue === undefined ? makeCall() : queue.then(makeCall, makeCall))
-        );
-        const clean = () => {
-          if (queues.get(key) === queue) {
-            queues.delete(key);
-          }
-        };
-        queue.then(clean, clean);
-        return queue;
-      };
-  },
-  (keyFunction = DEFAULT_KEY_FUNCTION) => {
-    const instancesQueues = new WeakMap();
-    return method =>
-      function() {
-        const key = keyFunction.apply(this, arguments);
-        let queues = instancesQueues.get(this);
-        if (queues === undefined) {
-          instancesQueues.set(this, (queues = new Map()));
-        }
-        const makeCall = () => method.apply(this, arguments);
-        let queue = queues.get(key);
-        queues.set(
-          key,
-          (queue =
-            queue === undefined ? makeCall() : queue.then(makeCall, makeCall))
-        );
-        const clean = () => {
-          if (queues.get(key) === queue) {
-            queues.delete(key);
-          }
-        };
-        queue.then(clean, clean);
-        return queue;
-      };
-  }
+  synchronize.withKey,
+  synchronizeMethod.withKey
 );
